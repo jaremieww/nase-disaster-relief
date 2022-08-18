b2f43d333e4cfb8f64fcccddc7914c8d18dc206c
Eric Andres <emandres@users.noreply.github.com>
2022-08-18 08:08:01 -0400

    overflow camping for Martin

--- src/components/east-ky-floods/MartinCC.tsx
+++ src/components/east-ky-floods/MartinCC.tsx
@@ -1,5 +1,6 @@
 import { Typography } from '@mui/material'
 import React from 'react'
+import PopoutLink from '../PopoutLink'
 import Assignments from './Assignments'
 import Contacts from './Contacts'
 import CrewLeaderChecklist from './CrewLeaderChecklist'
@@ -61,6 +62,14 @@ export default function MartinCC() {
     <Typography variant="h5" component="h2" sx={{ mb: 3 }}>Martin Command Center</Typography>
 
     <Contacts contacts={contacts} />
+    <p>
+      Overflow camping as available at:<br/>
+      <br/>
+      Stumbo Park Baseball Fields<br/>
+      <PopoutLink href="https://www.google.com/maps/place/Stumbo+Park/@37.6044099,-82.7293313,17z/data=!3m1!4b1!4m5!3m4!1s0x88450ff36c402d15:0x67947c3262568d36!8m2!3d37.6044057!4d-82.7271426">Stumbo Park Rd. Allen, KY 41601</PopoutLink><br/>
+      (approximately 5 miles from command center)
+    </p>
+
     <Assignments assignedStakes={assignedStakes} commandCenterName="Martin" />
     <CrewLeaderChecklist registrationLink='https://forms.gle/87JvxQTjchrYWrSn8' location='Martin, KY' />
     <ItemsOfConsideration />
