7537148fcb0c5f535f29445f2b4caab85fd95224
Eric Andres <emandres@users.noreply.github.com>
2022-09-28 22:16:23 -0400

    Fix hurricane Ian image on mobile

--- src/components/ian/Index.tsx
+++ src/components/ian/Index.tsx
@@ -9,7 +9,10 @@ export default () => {
     <Title level={2}>Hurricane Ian</Title>
 
     <Space direction="vertical">
-      <img src={`${S3}/ian/ian-satellite-800w.jpg`} alt="Hurricane Ian Satellite View" />
+      <img 
+        src={`${S3}/ian/ian-satellite-800w.jpg`} 
+        alt="Hurricane Ian Satellite View"
+        style={{maxWidth: '100%'}} />
       <cite>Image courtesy of <PopoutLink href="https://earthobservatory.nasa.gov/images/150408/hurricane-ian-reaches-florida">NASA</PopoutLink></cite>
     </Space>
 
