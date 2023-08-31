ed2f1e8444b32a411c042e535baf266cea7c794d
Eric Andres <emandres@gmail.com>
2023-08-31 15:27:03 -0400

    Spiff up index

--- src/components/idalia/Index.tsx
+++ src/components/idalia/Index.tsx
@@ -1,4 +1,4 @@
-import { List, Space, Typography } from "antd"
+import { List, Row, Space, Typography } from "antd"
 import { S3Link, formatS3Url } from "../S3"
 import PopoutLink from "../PopoutLink"
 
@@ -11,32 +11,34 @@ const files = [
 ]
 
 export default () => {
-  return <Space direction="vertical" size="large">
+  return <Space direction="vertical" size="large" style={{ width: '100%' }}>
     <Title level={2}>Hurricane Idalia</Title>
-
-    <Space direction="vertical">
-      <img
-        src={formatS3Url('idalia/idalia.jpeg')}
-        alt="Hurricane Idalia Satellite View"
-        style={{ maxWidth: 600 }} />
-      <cite>Image courtesy of <PopoutLink href="https://nhc.noaa.gov">NHC</PopoutLink></cite>
-    </Space>
-
-    <section>
-      <Title level={3}>Command Centers</Title>
+    <Space size="large" align="start">
       <Space direction="vertical">
-        <a href="/idalia/madison">Madison Command Center</a>
-        <a href="/idalia/lake-city">Lake City Command Center</a>
-        <a href="/idalia/chiefland">Chiefland Command Center</a>
+        <img
+          src={formatS3Url('idalia/idalia-satellite.jpeg')}
+          alt="Hurricane Idalia Satellite View"
+          style={{ maxWidth: 600 }} />
+        <cite>Image courtesy of <PopoutLink href="https://earthobservatory.nasa.gov">NASA Earth Observatory</PopoutLink></cite>
       </Space>
-    </section>
+      <Space direction="vertical" size="large">
+        <section>
+          <Title level={3}>Command Centers</Title>
+          <Space direction="vertical">
+            <a href="/idalia/madison">Madison Command Center</a>
+            <a href="/idalia/lake-city">Lake City Command Center</a>
+            <a href="/idalia/chiefland">Chiefland Command Center</a>
+          </Space>
+        </section>
 
-    <section>
-      <Title level={3}>Files</Title>
-      <List
-        dataSource={files}
-        renderItem={item => <List.Item><S3Link path={item.path}>{item.title}</S3Link></List.Item>}
-        rowKey={item => item.path} />
-    </section>
+        <section>
+          <Title level={3}>Files</Title>
+          <List
+            dataSource={files}
+            renderItem={item => <List.Item><S3Link path={item.path}>{item.title}</S3Link></List.Item>}
+            rowKey={item => item.path} />
+        </section>
+      </Space>
+    </Space>
   </Space>
 }
