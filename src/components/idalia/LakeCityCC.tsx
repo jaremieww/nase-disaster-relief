4788654022882fbe52b96f216338aaf0a589fd3f
Eric Andres <emandres@gmail.com>
2023-09-04 11:29:09 -0400

    Update Lake City director

--- src/components/idalia/LakeCityCC.tsx
+++ src/components/idalia/LakeCityCC.tsx
@@ -33,8 +33,8 @@ export default () => <CommandCenter
     }
   ]}
   director={{
-    name: 'Pres. Michael Lavoie',
-    phoneNumber: '(352) 672-0455'
+    name: 'Pres. Jeff Oldham',
+    phoneNumber: '(386) 361-3334'
   }}
   additionalInformation={
     <section>
