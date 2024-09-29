1e0cfdfc5977343948b8e1731d2c5fa848a3637d
jaremieww <156275759+jaremieww@users.noreply.github.com>
2024-09-29 16:11:09 -0400

    updated cc pages

--- src/components/helene/HLakeCityCC.tsx
+++ src/components/helene/HLakeCityCC.tsx
@@ -1,33 +1,29 @@
 import { s3Url } from '../../lib/s3';
-import PopoutLink from '../PopoutLink';
-import CommandCenter from "./CommandCenter";
+import CommandCenter from './CommandCenter';
 
-export default () => <CommandCenter
-  name="Command Center Two"
+export const HLakeCityCC = () => <CommandCenter 
   address={[]}
-  campingAddress={[]}
-  campingAddressNote="Camp site has plenty of room for RVs and campers"
-  email=""
-  assignedStakes={[]}
-  phoneNumber=""
-  registrationLink=""
-  closeoutFormLink=""
   areaOversight={[]}
   director={{
     name: '',
-    phoneNumber: ''
+    phoneNumber: '',
   }}
+  assignedStakes={[]}
+  email=''
+  name='Command Center Three'
+  phoneNumber=''
+  registrationLink=''
+  closeoutFormLink=''
+  campingAddress={[]}
+  sacramentLocation='command-center'
+  sacramentTime='7:00 AM'
   additionalInformation={
     <section>
       <h3>Command Center Layout</h3>
-      <img src={s3Url('/idalia/lake-city-command-center.jpg')} alt="Lake City Command Center Layout" style={{maxWidth: '100%'}} />
+      <img src={s3Url('/idalia/valdosta-cc-layout.jpg')} alt="Valdosta Command Center Layout" style={{maxWidth: '100%'}} />
       <h3>Campsite Map</h3>
-      <img src={s3Url('/idalia/lake-city-camping.png')} alt="Lake City Campsite Map" style={{maxWidth: '100%'}} />
+      <img src={s3Url('/idalia/valdosta-camping.png')} alt="Valdosta Campsite" style={{maxWidth: '100%'}} />
     </section>
   }
-  sacramentLocation='command-center'
-  sacramentTime='8:00 AM'
-  additionalTraining={<>
-    <li><PopoutLink href="https://sites.google.com/view/nase-command-center/home">Lake City Command Center Training</PopoutLink></li>
-  </>}
 />
+export default HValdostaCC
--- src/components/helene/HMadisonCC.tsx
+++ src/components/helene/HMadisonCC.tsx
@@ -1,18 +1,29 @@
-import CommandCenter from "./CommandCenter";
+import { s3Url } from '../../lib/s3';
+import CommandCenter from './CommandCenter';
 
-export default () => <CommandCenter
-  name="Command Center One"
+export const HMadisonCC = () => <CommandCenter 
   address={[]}
-  campingAddress={[]}
-  email=""
-  assignedStakes={[]}
-  phoneNumber=""
-  registrationLink=""
-  closeoutFormLink=""
   areaOversight={[]}
   director={{
     name: '',
-    phoneNumber: ''
+    phoneNumber: '',
   }}
-  sacramentLocation='campsite'
+  assignedStakes={[]}
+  email=''
+  name='Madison'
+  phoneNumber=''
+  registrationLink=''
+  closeoutFormLink=''
+  campingAddress={[]}
+  sacramentLocation='command-center'
+  sacramentTime='7:00 AM'
+  additionalInformation={
+    <section>
+      <h3>Command Center Layout</h3>
+      <img src={s3Url('/idalia/valdosta-cc-layout.jpg')} alt="Valdosta Command Center Layout" style={{maxWidth: '100%'}} />
+      <h3>Campsite Map</h3>
+      <img src={s3Url('/idalia/valdosta-camping.png')} alt="Valdosta Campsite" style={{maxWidth: '100%'}} />
+    </section>
+  }
 />
+export default HValdostaCC
