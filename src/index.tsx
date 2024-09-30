b68545dc7bfc2e64710d8b743931f70aa4a976ef
jaremieww <156275759+jaremieww@users.noreply.github.com>
2024-09-30 13:03:36 -0400

    update routing

--- src/index.tsx
+++ src/index.tsx
@@ -9,22 +9,18 @@ import Training from './Training'
 import CrewLeaderChecklist from './CrewLeaderChecklist'
 import CallCenterTrainingMaterials from './CallCenterTrainingMaterials'
 import CommandCenterTrainingMaterials from './CommandCenterTrainingMaterials'
-import IanIndex from './components/ian/Index'
-import PortCharlotteCC from './components/ian/PortCharlotteCC'
-import CapeCoralCC from './components/ian/CapeCoralCC'
-import VeniceCC from './components/ian/VeniceCC'
-import ValdostaCC from './components/ian/ValdostaCC'
-import MadisonCC from './components/ian/MadisonCC'
-import ChieflandCC from './components/ian/ChieflandCC'
-import WaycrossCC from './components/ian/WaycrossCC'
-import AshevilleCC from './components/ian/AshevilleCC'
-import DouglasCC from './components/ian/DouglasCC'
-import StPetersburgCC from './components/ian/StPetersburgCC'
-import LakeCityCC from './components/ian/LakeCityCC'
-import SavannahCC from './components/ian/SavannahCC'
-import AikenCC from './components/ian/AikenCC'
-import GreenvilleCC from './components/ian/GreenvilleCC'
-import LeniorCC from './components/ian/LeniorCC'
+import HeleneIndex from './components/helene/Index'
+import ValdostaCC from './components/helene/ValdostaCC'
+import MadisonCC from './components/helene/MadisonCC'
+import ChieflandCC from './components/helene/ChieflandCC'
+import WaycrossCC from './components/helene/WaycrossCC'
+import AshevilleCC from './components/helene/AshevilleCC'
+import DouglasCC from './components/helene/DouglasCC'
+import StPetersburgCC from './components/helene/StPetersburgCC'
+import LakeCityCC from './components/helene/LakeCityCC'
+import SavannahCC from './components/helene/SavannahCC'
+import AikenCC from './components/helene/AikenCC'
+import LeniorCC from './components/helene/LeniorCC'
 import CommunicationsTrainingMaterials from './CommunicationsTrainingMaterials';
 
 
@@ -41,13 +37,7 @@ ReactDOM.render(
           <Route path="communications" element={<CommunicationsTrainingMaterials />} />
           <Route path="command-center" element={<CommandCenterTrainingMaterials />} />
           <Route path="helene">
-            <Route index element={<IanIndex />} />         
-          </Route>
-          <Route path="ian">
-            <Route path="cape-coral" element={<CapeCoralCC />} />
-            <Route index element={<IanIndex />} />
-            <Route path="port-charlotte" element={<PortCharlotteCC />} />
-            <Route path="venice" element={<VeniceCC />} />
+            <Route index element={<HeleneIndex />} />
             <Route path="valdosta" element={<ValdostaCC />} />
             <Route path="madison" element={<MadisonCC />} />
             <Route path="chiefland" element={<ChieflandCC />} />
@@ -58,7 +48,6 @@ ReactDOM.render(
             <Route path="lakecity" element={<LakeCityCC />} />
             <Route path="savannah" element={<SavannahCC />} />
             <Route path="aiken" element={<AikenCC />} />
-            <Route path="greenville" element={<GreenvilleCC />} />
             <Route path="lenior" element={<LeniorCC />} />
           </Route>
           <Route index element={<Home />} />
