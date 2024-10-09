ee65413641ba75bf0e9b3d7057a6e0763d74945f
jaremieww <156275759+jaremieww@users.noreply.github.com>
2024-10-09 17:37:09 -0400

    call center link 4

--- src/CallCenterTrainingMaterials.tsx
+++ src/CallCenterTrainingMaterials.tsx
@@ -10,18 +10,6 @@ const { Title } = Typography
 export default function CallCenterTrainingMaterials() {
   return 
   <>
-    {/* <Title level={2}>Call Center Training Materials</Title>
-    <Space direction="vertical">
-      <PopoutLink href="https://docs.google.com/document/d/1VH9Ha17pUrznSEfVedwzbHcaAksDtZkd4Gxot2FHO_c/edit?usp=sharing">
-        <FaFile />
-          "2024 - NASE Phone Volunteer - Training Guide"
-      </PopoutLink>
-      <PopoutLink href="https://docs.google.com/document/d/1bWB7qaHWlzdb-YSvA1x-3S9ehfwKTDCaJDgCqRG6b98/edit">
-      <FaFile />
-          "2024 - NASE Stake Call Center Leadership Guide"
-      </PopoutLink>
-    </Space> */}
-           
     <DirectoryListing directory='call-center' />
   </>
 }
