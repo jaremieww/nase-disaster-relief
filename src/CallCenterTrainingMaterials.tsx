3356936911fc39d4dd97074ce0b2297ed731fe32
jaremieww <156275759+jaremieww@users.noreply.github.com>
2024-10-09 13:07:56 -0400

    fix link

--- src/CallCenterTrainingMaterials.tsx
+++ src/CallCenterTrainingMaterials.tsx
@@ -1,7 +1,7 @@
 import { Typography, Space } from 'antd'
 import DirectoryListing from './components/DirectoryListing'
 import PopoutLink from './components/PopoutLink'
-import {Icon, FileWordTwoTone } from '@ant-design/icons'
+import { Icon } from '@ant-design/icons'
 import {FaVideo} from 'react-icons/fa'
 
 
@@ -13,13 +13,13 @@ export default function CallCenterTrainingMaterials() {
     <Space direction="vertical">
     <PopoutLink href="https://docs.google.com/document/d/1VH9Ha17pUrznSEfVedwzbHcaAksDtZkd4Gxot2FHO_c/edit?usp=sharing">
       <Space>
-      <Icon component={FileWordTwoTone}/>
+      <Icon component={FaVideo}/>
         "2024 - NASE Phone Volunteer - Training Guide"
       </Space>
     </PopoutLink>
     <PopoutLink href="https://docs.google.com/document/d/1bWB7qaHWlzdb-YSvA1x-3S9ehfwKTDCaJDgCqRG6b98/edit">
       <Space>
-      <Icon component={FileWordTwoTone}/>
+      <Icon component={FaVideo}/>
         "2024 - NASE Stake Call Center Leadership Guide"
       </Space>
     </PopoutLink>
