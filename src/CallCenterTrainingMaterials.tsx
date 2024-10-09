1b5d1124f00e681fd2a1787d0ee1f04a51ec3cbd
jaremieww <156275759+jaremieww@users.noreply.github.com>
2024-10-09 15:37:51 -0400

    cc training

--- src/CallCenterTrainingMaterials.tsx
+++ src/CallCenterTrainingMaterials.tsx
@@ -1,6 +1,6 @@
 import { Typography, Space } from 'antd'
 import DirectoryListing from './components/DirectoryListing'
-import PopoutLink from './components/PopoutLink'
+//import PopoutLink from './components/PopoutLink'
 //import { Icon } from '@ant-design/icons'
 import {FaVideo} from 'react-icons/fa'
 
@@ -11,18 +11,25 @@ export default function CallCenterTrainingMaterials() {
   return <>
     <Title level={2}>Call Center Training Materials</Title>
     <Space direction="vertical">
-    <PopoutLink href="https://docs.google.com/document/d/1VH9Ha17pUrznSEfVedwzbHcaAksDtZkd4Gxot2FHO_c/edit?usp=sharing">
+    {/* <PopoutLink href="https://docs.google.com/document/d/1VH9Ha17pUrznSEfVedwzbHcaAksDtZkd4Gxot2FHO_c/edit?usp=sharing">
       <Space>
       <Icon component={FaVideo}/>
         "2024 - NASE Phone Volunteer - Training Guide"
       </Space>
-    </PopoutLink>
+    </PopoutLink> */}
     {/* <PopoutLink href="https://docs.google.com/document/d/1bWB7qaHWlzdb-YSvA1x-3S9ehfwKTDCaJDgCqRG6b98/edit">
       <Space>
       <Icon component={FaVideo}/>
         "2024 - NASE Stake Call Center Leadership Guide"
       </Space>
     </PopoutLink> */}
+
+    {/* <S3Link path={`${role.pathPrefix}/${file}`}>
+     <Space>
+        <Icon component={getIcon(file)} />
+        {file}
+     </Space>
+    </S3Link> */}
     </Space>
            
     <DirectoryListing directory='call-center' />
