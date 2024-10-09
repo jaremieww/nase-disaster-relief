cb9ab52e6ee8d6f270e96be637a3793b557c82ff
jaremieww <156275759+jaremieww@users.noreply.github.com>
2024-10-09 17:29:32 -0400

    call center

--- src/CallCenterTrainingMaterials.tsx
+++ src/CallCenterTrainingMaterials.tsx
@@ -1,8 +1,8 @@
-import { Typography, Space } from 'antd'
+import { Typography } from 'antd'
 import DirectoryListing from './components/DirectoryListing'
-import PopoutLink from './components/PopoutLink'
+//import PopoutLink from './components/PopoutLink'
 //import { Icon } from '@ant-design/icons'
-import { FaFile } from 'react-icons/fa'
+//import { FaFile } from 'react-icons/fa'
 
 
 const { Title } = Typography
@@ -10,7 +10,7 @@ const { Title } = Typography
 export default function CallCenterTrainingMaterials() {
   return 
   <>
-    <Title level={2}>Call Center Training Materials</Title>
+    {/* <Title level={2}>Call Center Training Materials</Title>
     <Space direction="vertical">
       <PopoutLink href="https://docs.google.com/document/d/1VH9Ha17pUrznSEfVedwzbHcaAksDtZkd4Gxot2FHO_c/edit?usp=sharing">
         <FaFile />
@@ -20,7 +20,7 @@ export default function CallCenterTrainingMaterials() {
       <FaFile />
           "2024 - NASE Stake Call Center Leadership Guide"
       </PopoutLink>
-    </Space>
+    </Space> */}
            
     <DirectoryListing directory='call-center' />
   </>
