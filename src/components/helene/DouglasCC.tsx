b9d98d31206f191fcfe6ddcd68890fad1a55effd
jaremieww <156275759+jaremieww@users.noreply.github.com>
2024-10-23 19:55:52 -0400

    zoom link

--- src/components/helene/DouglasCC.tsx
+++ src/components/helene/DouglasCC.tsx
@@ -26,6 +26,52 @@ export default () => <CommandCenter
       ]} />
     </>
   }
+  bigmessage = {
+    '
+
+Topic: Douglas Week 4 Team Captain Meeting
+Time: Oct 23, 2024 08:00 PM Eastern Time (US and Canada)
+
+Join Zoom Meeting
+https://zoom.us/j/94056543852?pwd=cyoEoZmwBx7x08Jac3kLaJ0EvPfMDZ.1
+
+Meeting ID: 940 5654 3852
+Passcode: 559921
+
+---
+
+One tap mobile
++13052241968,,94056543852#,,,,*559921# US
++16469313860,,94056543852#,,,,*559921# US
+
+---
+
+Dial by your location
+• +1 305 224 1968 US
+• +1 646 931 3860 US
+• +1 929 205 6099 US (New York)
+• +1 301 715 8592 US (Washington DC)
+• +1 309 205 3325 US
+• +1 312 626 6799 US (Chicago)
+• +1 386 347 5053 US
+• +1 507 473 4847 US
+• +1 564 217 2000 US
+• +1 669 444 9171 US
+• +1 669 900 6833 US (San Jose)
+• +1 689 278 1000 US
+• +1 719 359 4580 US
+• +1 253 205 0468 US
+• +1 253 215 8782 US (Tacoma)
+• +1 346 248 7799 US (Houston)
+• +1 360 209 5623 US
+
+Meeting ID: 940 5654 3852
+Passcode: 559921
+
+Find your local number: https://zoom.us/u/adLxOckRWZ
+
+  
+  }
   email="idastorm25@gmail.com"
   assignedStakes={[
     'Lake City FL *',
