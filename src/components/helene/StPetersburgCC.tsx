a5d9b4aa7cbfa0e8349b2454a24bd8ca959257f6
jaremieww <156275759+jaremieww@users.noreply.github.com>
2024-10-23 17:07:08 -0400

    st pete updates

--- src/components/helene/StPetersburgCC.tsx
+++ src/components/helene/StPetersburgCC.tsx
@@ -12,17 +12,15 @@ export default () => <CommandCenter
     'LARGO, Florida 33777'
   ]}
   campingAddress={[
-    '9001 106th Avenue',
+    '9400 98th Ave',
     'LARGO, Florida 33777'
   ]}
   campingAddressNote={
     <>
       RV and Heavy Equipment Parking<br />
       <Address address={[
-        'RV Tech Training Center',
-        '12949 66th St N',
-        'Largo, FL 33773', 
-        'This is a gravel lot, not ideal for tents'
+        '9001 106th Avenue',
+        'LARGO, Florida 33777'
       ]} />
     </>
   }
@@ -44,13 +42,13 @@ export default () => <CommandCenter
   registrationLink="https://forms.gle/Xu52Qap1XXbw5iaQ6"
   closeoutFormLink=""
   areaOversight={[{
-    name: 'Brent Barnes',
-    phoneNumber: '865-748-4260'
+    name: 'Mike Dohm',
+    phoneNumber: '985-718-9753'
   },
   ]}
   director={{
-    name: '',
-    phoneNumber: ''
+    name: 'Kasey Hess',
+    phoneNumber: '813-830-3223'
   }}
   afterAssignments={<>
     <p>
@@ -63,24 +61,23 @@ export default () => <CommandCenter
     additionalInformation={<>
       <section style={{ marginTop: 24 }}>
         <Title level={4}>Camping</Title>
-        <p>Camping for the St Peterburg Command Center is located at the curch: </p>
+        <p>Camping for the St Peterburg Command Center is located minutes away from the curch building at: </p>
+      <Address address={[
+        '9400 98th Avenue',
+        'LARGO, Florida 33777'
+      ]} />     
+      <p>Porta potties and a shower trailer will be provided at the campsite.  Egress and parking is limited at the campsite, so please no RVs and heavy equipment.<br/></p>
+      <p>RV and Heavy Equipment can park and camp at the church building. A toilet trailer is provided there.<br/></p>
       <Address address={[
         '9001 106th Avenue',
         'LARGO, Florida 33777'
-      ]} />     
-      <p>If we run out of room we will send people to the RV and Heavy Equipment Parking<br/></p>
-      <Address address={[
-        'RV Tech Training Center',
-        '12949 66th St N',
-        'Largo, FL 33773', 
-        'This is a gravel lot, not ideal for tents'
       ]} />
       </section>
     </>}
   sundayServices={<section style={{ marginTop: '40px' }}>
     <Title level={4}>Sunday Services</Title>
     <p>
-    A short Sacrament meeting will be held at the Command Center church building (9001 106th Avenue, Largo, FL 33777) on Sunday morning at 7:00 AM. Members should come in clean work clothes. Teams should carpool from the campsite as parking is limited at the church. Please no RVs and heavy equipment trailers. Following the meeting, crews should plan to continue working. 
+    A short Sacrament meeting will be held at the Command Center church building (9001 106th Avenue, Largo, FL 33777) on Sunday morning at 7:00 AM. Members should come in clean work clothes. Teams should carpool from the campsite as parking is limited at the church. Following the meeting, crews should plan to continue working. 
     </p>
   </section>}
    fuelAvailability="limited"
