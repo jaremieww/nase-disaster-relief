75871ee8e68a2cca3ba924ac2d9a157481a31b62
jaremieww <156275759+jaremieww@users.noreply.github.com>
2025-06-05 08:16:36 -0400

    Update FAQs.tsx

--- src/FAQs.tsx
+++ src/FAQs.tsx
@@ -5,6 +5,6 @@ const { Title } = Typography
 export default function FAQs() {
   return <>
     <Title level={2}>Frequently Asked Questions</Title>
-    
+    <p>Please look here to see some answers to the questions we most want to answer.</p>
   </>
 }
